a699d7ca7fd0505cd00c68caf015f5e1f80e0c31
Cai <caihongwei2008@gmail.com>
2014-05-13 10:04:08 +0800

    客户端修改

--- src/main/java/com/hong/cxf/client/HelloServiceClient.java
+++ src/main/java/com/hong/cxf/client/HelloServiceClient.java
@@ -1,22 +1,26 @@
 package com.hong.cxf.client;
 
-import com.hong.cxf.webservice.IHelloService;
-import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.endpoint.dynamic.DynamicClientFactory;
 
 /**
  * Created by Cai on 2014/5/12 16:08.
+ *
+ * CXF客户端测试
  */
 public class HelloServiceClient {
-    public static void main(String[] args) {
-//        ApplicationContext cxt = new ClassPathXmlApplicationContext("applicationContext-client.xml");
-//        IHelloService helloService = (IHelloService) cxt.getBean("client");
-//        String response = helloService.sayHello("Peter");
-//        System.out.println(response);
-        JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
-        factory.setServiceClass(IHelloService.class);
-        factory.setAddress("http://localhost:8080/cxf/ws/helloService");
-
+    public static void main(String[] args) throws Exception {
+//        String url = "http://192.168.9.72:8080/cxf/ws/helloService?wsdl";
+//        String method = "sayHello";
+        String url = "http://www.webxml.com.cn/webservices/qqOnlineWebService.asmx?wsdl";
+        String method = "qqCheckOnline";
+        DynamicClientFactory factory = DynamicClientFactory.newInstance();
+        Client client = factory.createClient(url);  // url：地址
+        Object reply = client.invoke(method, new Object[]{"327701898"});    // method：方法名称， Object[]：参数
+        Object[] replies = (Object[])reply;
+        System.out.println("length:" + replies.length);
+        for (Object o : replies) {
+            System.out.println(o);
+        }
     }
 }
